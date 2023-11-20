176ea7f7e5cffd172584d9b3bb0766b503b999c8
Kevin Pineda <Kevinpineda393@gmail.com>
2023-11-20 19:33:50 -0300

    segunda pre entrega

--- main.js
+++ main.js
@@ -1,67 +1,32 @@
-function comprar(){
 
 
-    let comprarEntradas = true
-    let maxEntradas = 4
-    let precioEntradas= 4500
-    
-    do{
-        let compra =  parseInt(prompt("Bienvenido! Ingresa cantidad de entradas que queres comprar (maximo de 4 por persona, solo efectivo)"))
-        if(compra === null){
-            break}
-        
-        if(compra <=4){
-            function multiplicar(numA, numB){
-                numA= compra
-                numB=precioEntradas
-                let resultado = numA * numB
-                alert("el total es " + resultado + "ARS")
-                let pago = parseInt(prompt("Con cuanto vas a pagar?"))
-                        function devolucion (num1, num2){
-                        num1= pago
-                        num2= resultado
-                        let devolucion= num1 - num2
+const persona =  function(nombre, edad, profesion) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.profesion = profesion;
+  }
+  
 
-                        if(pago<resultado){
-                            alert("Monto insuficiente para realizar el pago")
-                        }
-                        if(pago>resultado){
-                        alert("su vuelto es de "+ devolucion + "ARS Gracias por su compra.")
-                        
-                    }
-                        if(pago===resultado){
-                            alert("Gracias por el cambio y por su compra.")
-                        }
+  
+  let persona1= new persona("Juan",20, "medico")
+  let persona2= new persona("Lucas", 30,"residente")
+  let persona3= new persona("Pedro", 25, "kinesiologo")
+  let persona4= new persona("Luciana", 40, "radiologa")
+  let persona5= new persona("Sofia", 40,"tecnica")
+  let persona6= new persona("Ana", 40, "ingeniera")
 
-                        }
-                        devolucion()
-                        
-                        
-                comprarEntradas = false
-               
-                }
-       
-        }
-            
-            
-        
-         
-        else{
-            compra>4
-            alert("no podes comprar tantas entradas por persona")
-        }
-        
-        multiplicar()
-        }
-    
-    
-   
-    while(comprarEntradas)
-    
+  let invitados = [persona1, persona2, persona3, persona4, persona5, persona6]
+
+ 
+  function filtrarPersonas(){
+let busqueda = prompt("a quien buscas?").toLowerCase().trim()
+let resultado = invitados.filter((x)=>x.nombre.toLowerCase().includes(busqueda))
+
+if(resultado.length >0){
+    console.table(resultado)
+}
+else{
+    alert("no esta aca " + busqueda)
 }
 
-
-comprar()
-            
-            
-            
+}
