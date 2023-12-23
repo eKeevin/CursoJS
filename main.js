ecff840c28448c96427535b92f9ce3b484446136
Kevin Pineda <kevinpineda393@gmail.com>
2023-12-22 23:32:02 -0300

    proyecto finalizado

--- main.js
+++ main.js
@@ -1,141 +1,68 @@
- const Producto = function (nombre, precio, stock){
-    this.nombre = nombre
-    this.precio = precio
-    this.stock = stock
+const listaPokemon = document.querySelector("#listaPokemons");
+const botonHeader = document.querySelectorAll(".boton-header")
+let URL="https://pokeapi.co/api/v2/pokemon/";
+
+for (let i = 1; i <= 151; i++){
+    fetch(URL + i)
+        .then((response) => response.json())
+        .then(data => mostrarPoke(data))
 }
-    let producto1 = new Producto("Auriculares Redragon",20000,20)
-    let producto2 = new Producto("Auriculares Audiotechnica",60000,10)
-    let producto3 = new Producto("Teclado Redragon",15000,25)
-    let producto4 = new Producto("Teclado Zowie",30000,20)
-    let producto5 = new Producto("Mouse Logitech",15000,30)
-    let producto6 = new Producto("Mouse inalambrico Logitech",20000,20)
-    let producto7 = new Producto("Placa Video Nvidia 3200",80000,28)
-    let producto8 = new Producto("Placa de Video AMD",60000,20)
-    let producto9 = new Producto("Procesador Ryzen 5", 80000,10)
-    let producto10 = new Producto("Procesador Ryzen 7",100000,10)
 
-    let lista = [producto1, producto2, producto3, producto4,producto5,producto6,producto7,producto8,producto9,producto10]
-//seccion de storage
-    if(localStorage.getItem("productos")){
-        lista = JSON.parse(localStorage.getItem("productos"))
-    }else{
-        lista = lista
-    }
+function mostrarPoke(pokedata) {
 
-    // seccion de filtrado
+    let tipos = pokedata.types.map((x)=> `<p class="${x.type.name} tipo">${x.type.name}</p>`);
+    tipos = tipos.join('');
+    console.log(tipos)
 
-    function filtrarProductos (){
-        const body = document.querySelector("body")
-        const input = document.getElementById("filtrado").value
-        const valorIngresado = input.trim().toUpperCase() 
-        const resultado = lista.filter((x)=>x.nombre.toUpperCase().includes(valorIngresado)) 
 
-        const contenedorExistente = document.querySelector(".estiloContenedor");
-        if (contenedorExistente) {
-        contenedorExistente.remove();
-        }
-        
-        if(resultado.length > 0){
-            const contenedor = document.createElement ("div")
-            contenedor.classList.add("estiloContenedor")
-            resultado.forEach((x)=>{
-                    const card = document.createElement("div")
-                    const nombre = document.createElement("p")
-                    nombre.textContent = `producto: ${x.nombre}`
-                    card.appendChild(nombre)
-                    const precio = document.createElement("p")
-                    precio.textContent = `precio: ${x.precio}`
-                    card.appendChild(precio)
-                    const stock = document.createElement("p")
-                    stock.textContent = `en stock: ${x.stock}`
-                    card.appendChild(stock)
-                
-                contenedor.appendChild(card)
+console.log(tipos)
 
-            })
-
-            body.appendChild(contenedor)
-
-        }
-        else{
-            alert("no se encontro el producto")
-        }
-    }
-        const botonFiltrar = document.getElementById("filtrar")
-        botonFiltrar.classList.add("botoncitosBuscador")
-        botonFiltrar.addEventListener("click",filtrarProductos)
-        
-
-        //seccion de agregar un producto
-
-    function AgregarUnProducto(){
-    const form =  document.createElement("form")
-    form.classList.add("formularioNuevoProducto")  
-    form.innerHTML=
-    `<label for="producto-input">Producto:</label>
-    <input id= "producto-input" type="text" step="0.01" required>
+    const div = document.createElement("div");
+   div.classList.add("pokemon");
+    div.innerHTML=`
     
-    <label for="precio-input">Precio:</label>
-    <input id= "precio-input" type="number" step="0.01" required>
+    <p class="pokemon-id-back">#${pokedata.id}</p>
+    <div class="pokemon-imagen"><img src="${pokedata.sprites.other["official-artwork"].front_default}" alt="pikachu"></div>
+    <div class="pokemon-data">
+        <div class="nombre-contenedor">
+            <p class="pokemon-id">#${pokedata.id}</p>
+          <h2 class="pokemon-nombre">${pokedata.name}</h2>
+        </div>
+        <div class="pokemon-tipos">
+           ${tipos}
+        </div>
+        <div class="pokemon-stats">
+            <p class="pokemon-altura">${pokedata.height}m</p>
+            <p class="pokemon-peso">${pokedata.weight}kg</p>
+        </div>
 
-    <label for="stock-input">Stock:</label>
-    <input id= "stock-input" type="number" step="0.01" required>
+    </div>
 
-    <button type="submit">Agregar el producto</button> `
-    
-    form.addEventListener("submit", function(x){
-        x.preventDefault();
-        const productoInput = document.getElementById("producto-input").value.trim()
-        const precioInput = parseFloat (document.getElementById("precio-input").value)
-        const stockInput = parseInt (document.getElementById("stock-input").value)
-
-        if (isNaN(stockInput) || isNaN(precioInput)  || productoInput === ""){
-            alert("por favor ingresa valores validos.")
-            return
-            }
-        const producto = new Producto (productoInput, precioInput, stockInput)
-
-        if (lista.some((x)=>x.nombre === producto.nombre)){
-            alert ("ya existe ese producto")
-            return
-        }
-
-        lista.push(producto)
-
-        localStorage.setItem("productos", JSON.stringify(lista))
-        alert(`Agregaste el producto:${producto.nombre}`)
-
-        const contenedorDos = document.createElement ("div")
-        lista.forEach((x)=>{
-            const card = document.createElement("div")
-
-                    const nombre = document.createElement("p")
-                    nombre.textContent = `producto: ${x.nombre}`
-                    card.appendChild(nombre)
-
-                    const precio = document.createElement("p")
-                    precio.textContent = `precio: ${x.precio}`
-                    card.appendChild(precio)
-
-                    const stock = document.createElement("p")
-                    stock.textContent = `en stock: ${x.stock}`
-                    card.appendChild(stock)
-
-                
-                contenedorDos.appendChild(card)
-            })
-
-                const body = document.querySelector("body")
-                body.appendChild(contenedorDos)
-                form.reset()
-        })
-        
-        
-   
-    const body = document.querySelector("body")
-    body.appendChild(form)
+ `;
+ listaPokemon.append(div);
 }
-    const BotonAgregar= document.getElementById("BotonAP")
-    BotonAgregar.classList.add("botoncitosBuscador")
-    BotonAgregar.addEventListener("click",AgregarUnProducto)
-    
+
+botonHeader.forEach((boton) => {
+    boton.addEventListener("click", (event) => {
+        const botonId = event.currentTarget.id;
+        listaPokemon.innerHTML = "";
+
+        for (let i = 1; i <= 151; i++) {
+            fetch(URL + i)
+                .then((response) => response.json())
+                .then((data) => {
+
+                    if(botonId === "ver-todos"){
+                        mostrarPoke(data);
+                    }
+                    else{
+                    const tipos = data.types.map((x) => x.type.name);
+                    if (tipos.some((x) => x.includes(botonId))) {
+                        mostrarPoke(data);
+                        }
+                    }
+                })
+               
+        }
+    });
+})
